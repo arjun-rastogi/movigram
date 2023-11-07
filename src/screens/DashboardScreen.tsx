1ed42e5518337cb2afa3b8bb826542cb6cbcddb1
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-11-08 00:33:09 +0530

    User Profile Initialised

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,14 +1,15 @@
 import React, { useLayoutEffect } from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./HomeScreen";
 import ProfileScreen from "./ProfileScreen";
 import { View } from "react-native";
-import { StackScreenProps } from "@react-navigation/stack";
 import { Icon } from "react-native-elements";
-import { DrawerActions } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
-const DashboardScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
-  const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const DashboardScreen = () => {
+  const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -16,61 +17,32 @@ const DashboardScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
   }, []);
 
   return (
-    <>
-      <Drawer.Navigator initialRouteName="DashboardScreen">
-        <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "Home",
-            headerLeft: () => (
-              <View style={{ marginLeft: 10 }}>
-                <Icon
-                  name="bars"
-                  size={25}
-                  type="font-awesome"
-                  color="#777777"
-                  onPress={() =>
-                    navigation.dispatch(DrawerActions.openDrawer())
-                  }
-                />
-              </View>
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            title: "Profile",
-            headerLeft: () => (
-              <View style={{ marginLeft: 10 }}>
-                <Icon
-                  name="bars"
-                  size={25}
-                  type="font-awesome"
-                  color="#777777"
-                  onPress={() =>
-                    navigation.dispatch(DrawerActions.openDrawer())
-                  }
-                />
-              </View>
-            ),
-            headerRight: () => (
-              <View style={{ marginRight: 10 }}>
-                <Icon
-                  name="edit"
-                  size={25}
-                  type="font-awesome"
-                  color="#777777"
-                  onPress={() => navigation.navigate("Edit Profile")}
-                />
-              </View>
-            ),
-          }}
-        />
-      </Drawer.Navigator>
-    </>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <View style={{ marginLeft: 10 }}>
+              <Icon name="home" size={25} color={color} type="font-awesome" />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <View style={{ marginLeft: 10 }}>
+              <Icon name="user" size={25} color={color} type="font-awesome" />
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
