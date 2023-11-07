5b29af5692ad75c83195ed667d20fab267f33852
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-11-08 00:32:34 +0530

    user-profile initialised

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,74 +1,77 @@
-import React, { useLayoutEffect} from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './HomeScreen';
-import ProfileScreen from './ProfileScreen';
-import { View } from 'react-native';
-import { StackScreenProps } from '@react-navigation/stack';
-import { Icon } from 'react-native-elements';
-import { DrawerActions } from '@react-navigation/native';
+import React, { useLayoutEffect } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./HomeScreen";
+import ProfileScreen from "./ProfileScreen";
+import { View } from "react-native";
+import { StackScreenProps } from "@react-navigation/stack";
+import { Icon } from "react-native-elements";
+import { DrawerActions } from "@react-navigation/native";
 
 const DashboardScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
-
   const Drawer = createDrawerNavigator();
-  useLayoutEffect(()=>{
+  useLayoutEffect(() => {
     navigation.setOptions({
-        headerShown : false,
-    })
-  },[])
+      headerShown: false,
+    });
+  }, []);
 
- return (
+  return (
     <>
-    <Drawer.Navigator initialRouteName='DashboardScreen'>
-      <Drawer.Screen 
-      name="Home" 
-      component={HomeScreen} 
-      options={{
-        title: 'Home',
-        headerLeft: () => (
-          <View style={{marginLeft: 10}}>
-            <Icon
-              name="bars"
-              size={25}
-              type='font-awesome'
-              color="#777777"
-              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-            />
-          </View>
-        ),
-      }}
-      />
-      <Drawer.Screen
-      name="Profile" 
-      component={ProfileScreen} 
-      options={{
-        title: 'Profile',
-        headerLeft: () => (
-          <View style={{marginLeft: 10}}>
-            <Icon
-              name="bars"
-              size={25}
-              type='font-awesome'
-              color="#777777"
-              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-            />
-          </View>
-        ),
-        headerRight: () => (
-          <View style={{marginRight: 10}}>
-            <Icon
-              name="edit"
-              size={25}
-              type='font-awesome'
-              color="#777777"
-              onPress={() => navigation.navigate("Edit Profile")}
-            />
-          </View>
-        ),
-      }}
-      />
-    </Drawer.Navigator>
+      <Drawer.Navigator initialRouteName="DashboardScreen">
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Home",
+            headerLeft: () => (
+              <View style={{ marginLeft: 10 }}>
+                <Icon
+                  name="bars"
+                  size={25}
+                  type="font-awesome"
+                  color="#777777"
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.openDrawer())
+                  }
+                />
+              </View>
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            title: "Profile",
+            headerLeft: () => (
+              <View style={{ marginLeft: 10 }}>
+                <Icon
+                  name="bars"
+                  size={25}
+                  type="font-awesome"
+                  color="#777777"
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.openDrawer())
+                  }
+                />
+              </View>
+            ),
+            headerRight: () => (
+              <View style={{ marginRight: 10 }}>
+                <Icon
+                  name="edit"
+                  size={25}
+                  type="font-awesome"
+                  color="#777777"
+                  onPress={() => navigation.navigate("Edit Profile")}
+                />
+              </View>
+            ),
+          }}
+        />
+      </Drawer.Navigator>
     </>
   );
-}
+};
 
-export default DashboardScreen
+export default DashboardScreen;
--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
-import * as AuthSession from 'expo-auth-session';
-import { StackScreenProps } from '@react-navigation/stack';
-import { Button, Input, SocialIcon } from 'react-native-elements';
-import Icon  from 'react-native-vector-icons/FontAwesome';
-import * as Google from 'expo-auth-session/providers/google';
-import * as Facebook from 'expo-auth-session/providers/facebook';
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithCredential, FacebookAuthProvider } from 'firebase/auth';
-import { ResponseType } from 'expo-auth-session';
+import React, { useLayoutEffect } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { StackScreenProps } from "@react-navigation/stack";
+import { Button, Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 interface Props {
   handleLogin: (email: string, password: string) => void;
@@ -19,126 +15,79 @@ WebBrowser.maybeCompleteAuthSession();
 
 const SignupScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
   const [value, setValue] = React.useState({
-    email: '',
-    password: '',
-    error: ''
-  })
-
-  const signUp = async() => {
-    if (value.email === '' || value.password === '') {
-      setValue({
-        ...value,
-        error: 'Email and password are mandatory.'
-      })
-      return;
-    }
-    
-    try {
-      await createUserWithEmailAndPassword(auth, value.email, value.password);
-      navigation.navigate('Sign In');
-    } catch (err) {
-      
-      setValue({
-        ...value,
-        error: "Email Already in Use"
-      })
-    }
-  }
-
- 
-  const [reqGoogle, resGoogle, googlelogin] = Google.useIdTokenAuthRequest(
-    {
-      clientId: "926984366249-jp61kk42fpvjqdioi5o41thd74n64ej8.apps.googleusercontent.com",
-      iosClientId: "926984366249-b4cc95kqg1cqqjm32ufja4c6rppnroce.apps.googleusercontent.com",
-      androidClientId: "926984366249-ik4vekooe5cv8e2iihb4hu5fasmaei0r.apps.googleusercontent.com",
-    },
-  );
-
-
-  const [reqFb, resFb, fblogin] = Facebook.useAuthRequest({
-    responseType: ResponseType.Token,
-    clientId: '508108111436122',
-    androidClientId: "508108111436122",
-    iosClientId: "508108111436122",
+    email: "",
+    password: "",
+    error: "",
   });
 
-
-  React.useEffect(() => {
-    if (resGoogle?.type === 'success') {
-      const { id_token } = resGoogle.params;
-      const { accessToken } = resGoogle.params;
-      
-      const auth = getAuth();
-      const credential = GoogleAuthProvider.credential(id_token, accessToken);
-      signInWithCredential(auth, credential)
-      .then()
-      .catch(err => console.log(err));
-      }
-  }, [resGoogle]);
-
-  React.useEffect(() => {
-    if (resFb?.type === 'success') {
-      const { access_token } = resFb.params;
-      const auth = getAuth();
-      const credential = FacebookAuthProvider.credential(access_token);
-      signInWithCredential(auth, credential).then().catch(err => console.log(err));
+  const signUp = async () => {
+    if (value.email === "" || value.password === "") {
+      setValue({
+        ...value,
+        error: "Email and password are mandatory.",
+      });
+      return;
     }
-  }, [resFb]);
 
+    try {
+      await createUserWithEmailAndPassword(auth, value.email, value.password);
+      navigation.navigate("Sign In");
+    } catch (err) {
+      setValue({
+        ...value,
+        error: "Email Already in Use",
+      });
+    }
+  };
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View className='flex-1 justify-center items-center p-5'>
-      <Text className='text-lg'>Signup screen!</Text>
-      {!!value.error && <View className='text-white bg-red-500 p-3 mt-3'><Text>{value.error}</Text></View>}
+    <View className="flex-1 justify-center items-center p-5">
+      <Text className="text-lg">Signup screen!</Text>
+      {!!value.error && (
+        <View className="text-white bg-red-500 p-3 mt-3">
+          <Text>{value.error}</Text>
+        </View>
+      )}
 
       <Input
-        placeholder='Email'
-        className='ml-2 '
+        placeholder="Email"
+        label="Email"
+        className="ml-2 "
         value={value.email}
         onChangeText={(text) => setValue({ ...value, email: text })}
         keyboardType="email-address"
-        leftIcon={<Icon
-          name='envelope'
-          size={16}
-        />}
+        leftIcon={<Icon name="envelope" size={16} />}
       />
 
       <Input
-        placeholder='Password'
-        className='ml-2 '
+        placeholder="Password"
+        className="ml-2 "
         value={value.password}
         onChangeText={(text) => setValue({ ...value, password: text })}
         secureTextEntry={true}
-        leftIcon={<Icon
-          name='key'
-          size={16}
-        />}
+        leftIcon={<Icon name="key" size={16} />}
       />
 
-        <Button title="Signup" className='flex-1' onPress={signUp} />
-
-        <SocialIcon
-         onPress={() => {
-          googlelogin();
+      <Button title="Signup" className="flex-1" onPress={signUp} />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
         }}
-         disabled={!reqGoogle}
-         title='Sign in with google'
-         button
-         style={{padding: 20}} 
-         type='google'
-         />
- 
-        <SocialIcon
-         onPress={() => {
-          fblogin();
-        }}
-        disabled={!reqFb}
-         title='Sign up with facebbok'
-         button
-         style={{padding: 20}} 
-         type='facebook'
-         />
+      >
+        <Text>Already have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Sign In")}>
+          <Text style={{ color: "blue" }}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
